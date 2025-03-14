6bc35a6ce07522e2e619db445ce47db38eae77c6
grbalmeida <g.r.almeida@live.com>
2025-03-13 22:55:46 -0300

    O método PUT

--- secao03/main.py
+++ secao03/main.py
@@ -35,6 +35,14 @@ async def post_curso(curso: Curso):
     del curso.id
     return curso
 
+@app.put('/cursos/{curso_id}')
+async def put_curso(curso_id: int, curso: Curso):
+    if curso_id in cursos:
+        cursos[curso_id] = curso
+        del curso.id
+        return curso
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Não existe um curso com id {curso_id}')
 
 if __name__ == '__main__':
     import uvicorn
