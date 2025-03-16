c0694096d312fe87f5c3ab562031bf81f0bb0a3f
grbalmeida <g.r.almeida@live.com>
2025-03-16 17:24:16 -0300

    Revisando os docs

--- secao03/main.py
+++ secao03/main.py
@@ -1,63 +1,78 @@
-from typing import Any, Optional
+from typing import Any, Dict, List, Optional
 from fastapi import FastAPI, HTTPException, status, Response, Path, Query, Header, Depends
-from models import Curso
+from models import Curso, cursos
 from time import sleep
 
 def fake_db():
     try:
         print('Abrindo conexão com banco de dados...')
-        sleep(1)
+        sleep(0.1)
     finally:
         print('Fechando conexão com banco de dados...')
-        sleep(1)
+        sleep(0.1)
 
-app = FastAPI()
+app = FastAPI(
+    title='API de Cursos da Geek University',
+    version='0.0.1',
+    description='Uma API para estudo do FastAPI'
+)
 
-cursos = {
-    1: {
-        "titulo": "Programação para Leigos",
-        "aulas": 112,
-        "horas": 58
-    },
-    2: {
-        "titulo": "Algoritmos e Lógica de Programação",
-        "aulas": 87,
-        "horas": 67
-    }
-}
-
-@app.get('/cursos')
+@app.get('/cursos',
+    description='Retorna todos os cursos ou uma lista vazia.',
+    summary='Retorna todos os cursos',
+    response_model=List[Curso],
+    response_description='Cursos encontrados com sucesso.'
+)
 async def get_cursos(db: Any = Depends(fake_db)):
     return cursos
 
-@app.get('/cursos/{curso_id}')
+@app.get('/cursos/{curso_id}',
+    description='Obtém um curso por ID, se o curso não for encontrado, uma HTTPException com status 404 Not Found é lançada',
+    summary='Obtém um curso por ID'
+)
 async def get_curso(curso_id: int = Path(title='ID do curso', description='Deve ser entre 1 e 2', gt=0, lt=3),  db: Any = Depends(fake_db)):
     try:
-        curso = cursos[curso_id]
+        curso = next((c for c in cursos if c.id == curso_id), None)
         return curso
     except KeyError:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Curso não encontrado.')
     
-@app.post('/cursos', status_code=status.HTTP_201_CREATED)
+@app.post('/cursos',
+    status_code=status.HTTP_201_CREATED,
+    description='Adiciona um novo curso à lista de cursos',
+    summary='Adiciona um novo curso',
+    response_model=Curso
+)
 async def post_curso(curso: Curso, db: Any = Depends(fake_db)):
     next_id: int = len(cursos) + 1
-    cursos[next_id] = curso
-    del curso.id
+    curso.id = next_id
+    cursos.append(curso)
     return curso
 
-@app.put('/cursos/{curso_id}')
+@app.put('/cursos/{curso_id}',
+    description='Atualiza um curso existente',
+    summary='Atualiza um curso'
+)
 async def put_curso(curso_id: int, curso: Curso, db: Any = Depends(fake_db)):
-    if curso_id in cursos:
-        cursos[curso_id] = curso
-        del curso.id
-        return curso
+    curso_encontrado = next((c for c in cursos if c.id == curso_id), None)
+
+    if curso_encontrado:
+        curso_encontrado.titulo = curso.titulo
+        curso_encontrado.horas = curso.horas
+        curso_encontrado.aulas = curso.aulas
+        return curso_encontrado
     else:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Não existe um curso com id {curso_id}')
     
-@app.delete('/cursos/{curso_id}')
+@app.delete('/cursos/{curso_id}',
+    description='Exclui um curso existente',
+    summary='Exclui um curso'
+)
 async def delete_curso(curso_id: int, db: Any = Depends(fake_db)):
-    if curso_id in cursos:
-        del cursos[curso_id]
+    curso_encontrado = next((c for c in cursos if c.id == curso_id), None)
+
+    if curso_encontrado:
+        cursos.remove(curso_encontrado)
         return Response(status_code=status.HTTP_204_NO_CONTENT)
     else:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Não existe um curso com id {curso_id}')
--- secao03/models.py
+++ secao03/models.py
@@ -7,3 +7,9 @@ class Curso(BaseModel):
     titulo: str
     aulas: int
     horas: int
+
+
+cursos = [
+    Curso(id=1, titulo='Programação para Leigos', aulas=42, horas=56),
+    Curso(id=2, titulo='Algoritmos e Lógica de Programação', aulas=52, horas=66)
+]
