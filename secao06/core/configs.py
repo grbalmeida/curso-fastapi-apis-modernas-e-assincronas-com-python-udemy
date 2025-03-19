9b51dab8f6aad7ae0c7d366247fb8d00ded8f974
grbalmeida <g.r.almeida@live.com>
2025-03-19 14:56:45 -0300

    Criando as tabelas

--- secao06/core/configs.py
+++ secao06/core/configs.py
@@ -1,11 +1,11 @@
-from typing import List
+from typing import ClassVar, List
 from pydantic_settings import BaseSettings
 from sqlalchemy.ext.declarative import declarative_base
 
 class Settings(BaseSettings):
     API_V1_STR: str = '/api/v1'
     DB_URL: str = 'postgresql+asyncpg://geek:university@localhost:5432/faculdade'
-    DBBaseModel = declarative_base()
+    DBBaseModel: ClassVar = declarative_base()
     # TODO: Usar env
     JWT_SECRET: str = 'k_FL7OmpsyayRYT7xMn7JMJD8XqjXsc4uK_ejfmbpFw'
     ALGORITHM: str = 'HS256'
