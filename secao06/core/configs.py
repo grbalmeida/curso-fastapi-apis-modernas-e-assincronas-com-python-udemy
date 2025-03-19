ec95789f70fb25b9fd6884122862f7561f6a7df3
grbalmeida <g.r.almeida@live.com>
2025-03-19 12:33:18 -0300

    Trabalhando no módulo core - Parte 1

---
+++ secao06/core/configs.py
@@ -0,0 +1,18 @@
+from typing import List
+from pydantic_settings import BaseSettings
+from sqlalchemy.ext.declarative import declarative_base
+
+class Settings(BaseSettings):
+    API_V1_STR: str = '/api/v1'
+    DB_URL: str = 'postgresql+asyncpg://geek:university@localhost:5432/faculdade'
+    DBBaseModel = declarative_base()
+    # TODO: Usar env
+    JWT_SECRET: str = 'k_FL7OmpsyayRYT7xMn7JMJD8XqjXsc4uK_ejfmbpFw'
+    ALGORITHM: str = 'HS256'
+    # 60 minutos * 24 horas * 7 dias => 1 semana
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 7
+
+    class Config:
+        case_sensitive = True
+
+settings: Settings = Settings()
