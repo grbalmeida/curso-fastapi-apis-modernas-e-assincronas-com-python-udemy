de36c00e032fe12335c8c84e139ceabe67d8ca47
grbalmeida <g.r.almeida@live.com>
2025-03-21 13:30:03 -0300

    Trabalhando no main

--- secao06/schemas/usuario_schema.py
+++ secao06/schemas/usuario_schema.py
@@ -19,8 +19,8 @@ class UsuarioSchemaArtigos(UsuarioSchemaBase):
     artigos: Optional[List[ArtigoSchema]]
 
 class UsuarioSchemaUp(UsuarioSchemaBase):
-    nome: Optional[str]
-    sobrenome: Optional[str]
-    email: Optional[EmailStr]
-    senha: Optional[str]
-    eh_admin = Optional[bool]    
+    nome: Optional[str] = None
+    sobrenome: Optional[str] = None
+    email: Optional[EmailStr] = None
+    senha: Optional[str] = None
+    eh_admin: Optional[bool] = None
